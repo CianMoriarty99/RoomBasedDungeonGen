913c8dee4234e4e74cf06ff8445510169fe9c284
CianMoriarty99 <chenju5511@gmail.com>
2020-06-22 15:13:48 +0100

    Added enemies and player spawn

--- Grid.cs
+++ Grid.cs
@@ -8,21 +8,41 @@ public class Grid : MonoBehaviour
     public Node[,] grid;
     public GameObject floorTile;
     public GameObject wallTile;
+    public GameObject enemyPrefab;
+    public GameObject playerPrefab;
     public int worldSize;
     public int roomSize;
     public int numberOfRooms;
+
+    public int maxMobsPerRoom;
     
     void Start()
     {
-        worldSize = numberOfRooms * roomSize + 1;
+        worldSize = numberOfRooms * roomSize *2;
         InitialiseGrid();
         GenerateDungeon(numberOfRooms, roomSize);
         FixWalls();
+        PlaceTiles();
     }
 
     void Update()
     {
         
+    }
+
+    void InitialiseGrid(){
+        grid = new Node[worldSize,worldSize];
+
+        for (int x = 0; x < worldSize; x++){ 
+			for (int y = 0; y < worldSize; y++){
+                grid[x,y] = new Node(0,0);
+                
+            }
+        }
+
+        //Add Player 
+        int j = 0 + worldSize/2 + roomSize/2;
+        grid[j,j].occupied = 1;
     }
 
     void GenerateDungeon(int numberOfRooms, int roomSize){
@@ -39,8 +59,6 @@ public class Grid : MonoBehaviour
                 i++;
             }
 
-            
-            
         int r = Random.Range(1,5); // up, down, left, right
 
         switch (r)
@@ -66,7 +84,6 @@ public class Grid : MonoBehaviour
         }
 
 
-
     }
 
 
@@ -74,11 +91,6 @@ public class Grid : MonoBehaviour
 
         int posX = (int)pos.x;
         int posY = (int)pos.y;
-        //Debug.Log(size);
-        //Debug.Log(posX);
-        //Debug.Log(posY);
-
-        // int modulo = size % 2;
 
 
         for (int x = posX; x < (posX + size); x++){
@@ -86,16 +98,13 @@ public class Grid : MonoBehaviour
             if(x != (posX + size/2)){
             //Bottom wall of room
                 if (grid[x, posY].tileType == 0) {
-
                     grid[x, posY].tileType = 2;
-                    Instantiate(wallTile, new Vector3(x, posY,0) , Quaternion.identity);
 
                 }
 
             //Top wall of room
-                if (grid[x, posY + size - 1].tileType == 0) {
-                    Instantiate(wallTile, new Vector3(x, posY + size-1 ,0) , Quaternion.identity);
-                    grid[x, posY + size - 1].tileType = 2;
+                if (grid[x, posY + size -1].tileType == 0) {
+                    grid[x, posY + size -1].tileType = 2;
                 }
             }
 
@@ -106,13 +115,11 @@ public class Grid : MonoBehaviour
 
                     //Left wall of room
                     if (grid[posX, y].tileType == 0) {
-                        Instantiate(wallTile, new Vector3(posX, y , 0) , Quaternion.identity);
                         grid[posX, y].tileType = 2;
                     }
 
                     //Right wall of room
-                    if (grid[posX + size-1, y].tileType == 0) {
-                        Instantiate(wallTile, new Vector3(posX + size-1, y , 0) , Quaternion.identity);     
+                    if (grid[posX + size-1, y].tileType == 0) {   
                         grid[posX + size-1, y ].tileType = 2;
                     }
                 }
@@ -120,43 +127,83 @@ public class Grid : MonoBehaviour
                     //Rest is floor
                     if(grid[x,y].tileType == 0){
                         grid[x,y].tileType = 1;
-                        Instantiate(floorTile, new Vector3(x,y,0) , Quaternion.identity);
+
                     }
                     
 
                 }
-            
         }
 
+        if(pos != new Vector2(worldSize/2,worldSize/2)){
 
+            //Spawn enemies per room
+            int m = Random.Range(0,maxMobsPerRoom);
+            Debug.Log(m);
 
+            for(int i = 0; i < m; i++)
+            {
+                //Debug.Log("How Many Times");
+                int randX = Random.Range(posX + 1,posX - 1 + size);
+                int randY = Random.Range(posY + 1,posY - 1 + size);
+
+                if(grid[randX,randY].tileType != 2){
+                    grid[randX,randY].occupied = 2;
+                    
+                    
+                    
+                }
+
+            }
+        }
         
+
+            
+        
+
     }
 
     void FixWalls(){
 
-        for (int x = 0; x < worldSize; x++){ 
-			for (int y = 0; y < worldSize; y++){
+        for (int x = 0; x < worldSize - 1; x++){ 
+			for (int y = 0; y < worldSize ; y++){
                 
                 if (grid[x,y].tileType == 1)
                     if(grid[x+1,y].tileType == 0 || grid[x-1,y].tileType == 0 || grid[x,y+1].tileType == 0 || grid[x,y-1].tileType == 0){
                         grid[x,y].tileType = 2;
-                        Instantiate(wallTile, new Vector3(x,y,0) , Quaternion.identity);
                     }
+
+                
+
             }
         }
     }
 
-    void InitialiseGrid(){
-        grid = new Node[worldSize,worldSize];
+    
 
+
+    void PlaceTiles(){
+        
         for (int x = 0; x < worldSize; x++){ 
 			for (int y = 0; y < worldSize; y++){
-                grid[x,y] = new Node(0,0);
+
+                if(grid[x,y].tileType == 1)
+                    Instantiate(floorTile, new Vector2(x,y), Quaternion.identity);
+
+                if(grid[x,y].tileType == 2)
+                    Instantiate(wallTile, new Vector2(x,y), Quaternion.identity);
                 
+                if(grid[x,y].occupied == 1)
+                    Instantiate(playerPrefab, new Vector2(x,y), Quaternion.identity);
+
+                if(grid[x,y].occupied == 2)
+                    Instantiate(enemyPrefab, new Vector2(x,y), Quaternion.identity);
+
             }
         }
+
     }
+
+
 
 
 }
